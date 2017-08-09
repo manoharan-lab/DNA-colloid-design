fd0796aa6c70abe676231491c5ec88e9b64e2c6f
egehrels <egehrels@fas.harvard.edu>
2017-08-09 10:39:29 -0400

    copyright

--- reverser.m
+++ reverser.m
@@ -1,5 +1,23 @@
+% Copyright 2017, Vinothan N. Manoharan, Emily W. Gehrels
+%
+% This file is part of DNA-colloid-design.
+%
+% DNA-colloid-design is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% DNA-colloid-design is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with DNA-colloid-design.  If not, see <http://www.gnu.org/licenses/>.
+
 function [ revSeq ] = reverser( seq )
-%UNTITLED3 Summary of this function goes here
+%this takes a sequence and finds it's reversed complement
+
 
 leng=length(seq);
 ii=0;
