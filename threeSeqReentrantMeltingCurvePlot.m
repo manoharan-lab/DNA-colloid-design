bd2288c8e11403eb01a7db515375eb1da7ab98da
egehrels <egehrels@fas.harvard.edu>
2017-08-09 10:40:06 -0400

    copyright

--- threeSeqReentrantMeltingCurvePlot.m
+++ threeSeqReentrantMeltingCurvePlot.m
@@ -1,3 +1,20 @@
+% Copyright 2017, Vinothan N. Manoharan, Emily W. Gehrels
+%
+% This file is part of DNA-colloid-design.
+%
+% DNA-colloid-design is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% DNA-colloid-design is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with DNA-colloid-design.  If not, see <http://www.gnu.org/licenses/>.
+
 function f=threeSeqReentrantMeltingCurvePlot(seqA, seqD1, Cd1, saltConc)
 
     tempStep = 0.25;
